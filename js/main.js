48a6f64111bc1ad19f8a0d64387e37e747d5cc18
Eugene Shlapakov <eugene.shlapakov@gmail.com>
2020-09-28 04:29:07 +0300

    Updated navbar work on small devices

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').addClass('active');
+
         } else {
             $('.navbar').removeClass('active');
         }
